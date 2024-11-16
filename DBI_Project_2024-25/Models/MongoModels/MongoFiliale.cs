cdab01256f2f12a85f0675c7c34e4f6035d8c4bb
Mark Liteykin <liteykin.mark@gmail.com>
2024-11-16 14:28:51 +0100

    Created Mongo classes

---
+++ DBI_Project_2024-25/Models/MongoModels/MongoFiliale.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DBI_Project_2024_25.Models.MongoModels;
+
+public class MongoFiliale
+{
+    public MongoFiliale(string id, string name, string adresse)
+    {
+        this.id = id;
+        Name = name;
+        Adresse = adresse;
+    }
+
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string id { get; set; } = default!;
+
+    [BsonElement("name")] public string Name { get; set; } = default!;
+    [BsonElement("addresse")] public string Adresse { get; set; } = default!;
+    [BsonElement("tierFilialen")] public ICollection<MongoTierFiliale> TierFiliale { get; set; } = new List<MongoTierFiliale>();
+}
---
+++ DBI_Project_2024-25/Models/MongoModels/MongoTier.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DBI_Project_2024_25.Models.MongoModels;
+
+public class MongoTier
+{
+    public MongoTier()
+    {
+    }
+
+    public MongoTier(string name, decimal groesse, decimal gewicht)
+    {
+        Name = name;
+        Groesse = groesse;
+        Gewicht = gewicht;
+    }
+
+    [BsonId] [BsonElement("name")] public string Name { get; set; } = default!;
+    [BsonElement("groesse")] public decimal Groesse { get; set; } = -1;
+    [BsonElement("gewicht")] public decimal Gewicht { get; set; } = -1;
+    [BsonElement("tierFilialen")] public ICollection<MongoTierFiliale> TierFilialen { get; set; } = new List<MongoTierFiliale>();
+}
